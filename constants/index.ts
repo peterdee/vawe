fce6bbbb1cb9ed80ca719e13b45ac77df21e12e2
Peter <p.dyumin@dunice.net>
2024-08-02 15:28:53 +0300

    Work on Websockets

--- constants/index.ts
+++ constants/index.ts
@@ -44,5 +44,8 @@ export const WS_EVENTS = {
   onTimeUpdate: 'ON_TIME_UPDATE',
   loadPlaylist: 'LOAD_PLAYLIST',
   removeTrack: 'REMOVE_TRACK',
+  requestCurrentTrack: 'REQUEST_CURRENT_TRACK',
+  requestPlaybackState: 'REQUEST_PLAYBACK_STATE',
+  requestPlaylist: 'REQUEST_PLAYLIST',
   updatePlaybackState: 'UPDATE_PLAYBACK_STATE',
 };
